01a4b1ceebf3acfcca6f453ddc2063a0845b09ee
kaikeventura <kaike.gnr@hotmail.com>
2025-12-26 18:04:18 -0300

    first commit

---
+++ app/src/main/java/com/kaikeventura/app_automatic_pipeline/AppAutomaticPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.app_automatic_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAutomaticPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAutomaticPipelineApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/kaikeventura/app_automatic_pipeline/AppAutomaticPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.app_automatic_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAutomaticPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
